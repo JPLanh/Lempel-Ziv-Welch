c5677b5f479be184636d23f2ee61e601f123c84f
JPLanh <JPLanh@yahoo.com>
2018-04-17 10:07:12 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from base64 import b64encode
 
 nodesSet = []
 
-for i in range(0, 5):
-  nodesSet.append(node.Node())
+node.Node(0)
+for i in range(1, 6):
+  focusNode = node.Node(i)
+  nodesSet.append(focusNode)
+  focusNode.joinRing(nodesSet)
+
+for i in nodesSet:
+  i.printInfo()
+  
+finger = nodesSet[0]
   
 MR = mapReduce.map(nodesSet)
 
@@ -14,5 +22,7 @@ data = f.read()
 
 i = 0
 while i < len(data):
-  MR.mapping(data[i:i+4096])
+  MR.mapping(data[i:i+4096], finger)
   i+=4096
+
+MR.emit(finger)
